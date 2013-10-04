2ef89d1156f06710db8500bd97b730135c3f6883
Andrew <andronavt13@gmail.com>
2013-10-04 17:05:24 +0100

    add archetype

---
+++ First/src/main/java/Test/First/repo/MemberDao.java
@@ -0,0 +1,16 @@
+package Test.First.repo;
+
+import java.util.List;
+
+import Test.First.domain.Member;
+
+public interface MemberDao
+{
+    public Member findById(Long id);
+
+    public Member findByEmail(String email);
+
+    public List<Member> findAllOrderedByName();
+
+    public void register(Member member);
+}
