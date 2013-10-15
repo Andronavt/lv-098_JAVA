f9c33ccb0d46ba1a24c9b27782e2be116e366492
Andrew <andronavt13@gmail.com>
2013-10-15 17:43:31 +0300

    new

--- src/main/java/Test/First/domain/Ip.java
+++
@@ -1,7 +0,0 @@
-package Test.First.domain;
-
-public interface Ip {
-	public String getIp();
-	public void setAddress(String address);
-	public String getAddress();
-}
--- src/main/java/Test/First/domain/ParserInterface.java
+++
@@ -1,8 +0,0 @@
-package Test.First.domain;
-
-import java.io.IOException;
-import java.util.List;
-
-public interface ParserInterface {
-	public List<Ip> parse() throws IOException;
-}
