745f2ba8bc9056a94d4cb0eb06ae7a424fdc99dc
Orest <orecto@gmail.com>
2013-10-15 19:18:48 +0300

    test

--- src/main/java/tc/lv/domain/IpV4Address.java
+++ src/main/java/tc/lv/domain/IpV4Address.java
@@ -9,6 +9,7 @@ import javax.persistence.Table;
 @Entity
 @SequenceGenerator(initialValue = 1, name = "idGenerator")
 @Table(name = "ipv4_addresses")
+//test comment
 public class IpV4Address extends IpAddress {
 
 	public IpV4Address() {
