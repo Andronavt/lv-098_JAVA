3d6fbb8e41e0d36ace06f11bb826cb8b75b74a6a
Andrew <andronavt13@gmail.com>
2013-11-01 22:38:06 +0200

    fix

--- src/main/java/tc/lv/web/SourceController.java
+++ src/main/java/tc/lv/web/SourceController.java
@@ -8,6 +8,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import tc.lv.domain.Source;
