d0417ec11bc5416255c1c9b36cc2014036c17ea3
Bohdan <bkolesnyk85@gmail.com>
2013-11-07 14:05:56 +0200

    repair after pull from olkooo

--- src/main/java/tc/lv/utils/ParserChaosreignsWL.java
+++ src/main/java/tc/lv/utils/ParserChaosreignsWL.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
 import java.util.regex.Matcher;
