4800574887988db7895b0d50d6084797156c7127
Bohdan <bkolesnyk85@gmail.com>
2013-11-08 20:22:49 +0200

    last project

--- src/main/java/tc/lv/service/SourceServiceImpl.java
+++ src/main/java/tc/lv/service/SourceServiceImpl.java
@@ -19,58 +19,57 @@ import tc.lv.exceptions.DBCreateSourceException;
 @Service
 public class SourceServiceImpl implements SourceService {
 
-    @Autowired
-    private SourceDao sourceDao;
-    @Autowired
-    private IpV4AddressDao ipV4AddressDao;
-    @Autowired
-    private IpV6AddressDao ipV6AddressDao;
-   
-    private WhiteListDao whiteListDao;
-    
-    private BlackListDao blackListDao;
+        @Autowired
+        private SourceDao sourceDao;
+        @Autowired
+        private IpV4AddressDao ipV4AddressDao;
+        @Autowired
+        private IpV6AddressDao ipV6AddressDao;
+        @Autowired
+        private WhiteListDao whiteListDao;
+        @Autowired
+        private BlackListDao blackListDao; 
+        
+        @Transactional
+        public List<IpV4Address> getIpV4ListFromSource(int sourceId) {
+                return ipV4AddressDao.loadAll(sourceId);
+        }
 
-    @Transactional
-    public List<IpV4Address> getIpV4ListFromSource(int sourceId) {
-	return ipV4AddressDao.loadAll(sourceId);
-    }
+        @Transactional
+        public void setIpV4AddressToWl(String ip, int sourceId) {
+            whiteListDao.saveIpV4(ip);
+        }
+        
+        @Transactional
+        public void setIpV4AddressToBl(String ip, int sourceId) {
+            blackListDao.saveIpV4(ip);
+        }
 
-    @Transactional
-    public void setIpV4Address(String ip, int sourceId) {
-	whiteListDao.saveIpV4(ip);
-    }
-    
-	@Transactional
-	public void setIpV4AddressToBl(String ip, int sourceId) {
-	    blackListDao.saveIpV4(ip);
-	}
+        @Transactional
+        public void setIpV6AddressToWl(String ip, int sourceId) {
+                whiteListDao.saveIpV6(ip);
+        }
+        
+        @Transactional
+        public void setIpV6AddressToBl(String ip, int sourceId) {
+                blackListDao.saveIpV6(ip);
+        }
 
-	@Transactional
-	public void setIpV6AddressToWl(String ip, int sourceId) {
-		whiteListDao.saveIpV6(ip);
-	}
-	
-	@Transactional
-	public void setIpV6AddressToBl(String ip, int sourceId) {
-		blackListDao.saveIpV6(ip);
-	}
+        @Transactional
+        public void addNewFeed(String dirname, String listType, double rank,
+                        String sourceName) throws DBCreateSourceException {
+                Source tempSource = new Source(sourceName, new Date(), rank, dirname, listType);
+                
+                        sourceDao.create(tempSource);
+        }
 
- 
-	@Transactional
-	public void addNewFeed(String dirname, String listType, double rank,
-			String sourceName) throws DBCreateSourceException {
-		Source tempSource = new Source(sourceName, new Date(), rank, dirname, listType);
-		
-			sourceDao.create(tempSource);
-	}
+        @Transactional
+        public List<Source> getListOfSourcess() {
+                return sourceDao.loadAll();
+        }
 
-    @Transactional
-    public List<Source> getListOfSourcess() {
-	return sourceDao.loadAll();
-    }
-
-    @Transactional
-    public void deleteFeed(String sourceName) {
-	sourceDao.delete(sourceName);
-    }
+        @Transactional
+        public void deleteFeed(String sourceName) {
+                sourceDao.delete(sourceName);
+        }
 }
