a1db0378c4ab85c585ccd67afed952309b5227a7
Oleg Yatskiv <olegyatskiv@gmail.com>
2013-11-11 17:15:47 +0200

    WORK

--- src/main/java/tc/lv/web/SigninController.java
+++ src/main/java/tc/lv/web/SigninController.java
@@ -13,24 +13,6 @@ public class SigninController {
 
     @RequestMapping(value = "/signin-failure", method = RequestMethod.GET)
     public String signinFailure() {
-	return "signin_failure";
-    }
-}
-package tc.lv.web;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-@Controller
-public class SigninController {
-    @RequestMapping(value = "/signin", method = RequestMethod.GET)
-    public String signin() {
-	return "signin";
-    }
-
-    @RequestMapping(value = "/signin-failure", method = RequestMethod.GET)
-    public String signinFailure() {
 	return "signinFailure";
     }
 }
