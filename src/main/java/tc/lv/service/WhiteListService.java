0d144237d88c830d02c806d557ab58ec6ee4c531
Oleg Yatskiv <olegyatskiv@gmail.com>
2013-11-12 17:05:39 +0200

    New Services

--- src/main/java/tc/lv/service/WhiteListService.java
+++ src/main/java/tc/lv/service/WhiteListService.java
@@ -8,33 +8,27 @@ import tc.lv.exceptions.DBException;
 import tc.lv.exceptions.DBIllegalArgumentException;
 import tc.lv.exceptions.DBIllegalStateException;
 import tc.lv.exceptions.DBPersistanceException;
+import tc.lv.exceptions.WhiteListServiceException;
 
 public interface WhiteListService {
-    public void deleteIpV4(String address) throws DBPersistanceException,
-	    DBIllegalArgumentException, DBIllegalStateException, DBException;
 
-    public void deleteIpV6(String address) throws DBPersistanceException,
-	    DBIllegalArgumentException, DBIllegalStateException, DBException;
+	public void deleteIpV4(String address) throws WhiteListServiceException;
 
-    public void saveIpV4(String address) throws DBPersistanceException,
-	    DBIllegalArgumentException, DBIllegalStateException, DBException;
+	public void deleteIpV6(String address) throws WhiteListServiceException;
 
-    public void saveIpV6(String address) throws DBPersistanceException,
-	    DBIllegalArgumentException, DBIllegalStateException, DBException;
+	public void saveIpV4(String address) throws WhiteListServiceException;
 
-    public Collection<IpV4Address> loadIpV4List()
-	    throws DBPersistanceException, DBIllegalArgumentException,
-	    DBIllegalStateException, DBException;
+	public void saveIpV6(String address) throws WhiteListServiceException;
 
-    public Collection<IpV6Address> loadIpV6List()
-	    throws DBPersistanceException, DBIllegalArgumentException,
-	    DBIllegalStateException, DBException;
+	public Collection<IpV4Address> loadIpV4List()
+			throws WhiteListServiceException;
 
-    public Collection<IpV4Address> loadIpV4ListByRange(int from, int count)
-	    throws DBPersistanceException, DBIllegalArgumentException,
-	    DBIllegalStateException, DBException;
+	public Collection<IpV6Address> loadIpV6List()
+			throws WhiteListServiceException;
 
-    public Collection<IpV6Address> loadIpV6ListByRange(int from, int count)
-	    throws DBPersistanceException, DBIllegalArgumentException,
-	    DBIllegalStateException, DBException;
+	public Collection<IpV4Address> loadIpV4ListByRange(int from, int count)
+			throws WhiteListServiceException;
+
+	public Collection<IpV6Address> loadIpV6ListByRange(int from, int count)
+			throws WhiteListServiceException;
 }
