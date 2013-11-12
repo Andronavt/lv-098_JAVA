d81ad3520b155f133dedf7bcbab238fdc0410879
Bohdan <bkolesnyk85@gmail.com>
2013-11-12 17:09:53 +0200

    commit

--- src/main/java/tc/lv/exceptions/DBException.java
+++ src/main/java/tc/lv/exceptions/DBException.java
@@ -1,19 +1,14 @@
 package tc.lv.exceptions;
 
 public class DBException extends IPException {
-	/**
+    /**
      * 
      */
-	private static final long serialVersionUID = -2943018141412512379L;
+    private static final long serialVersionUID = -2943018141412512379L;
 
-	public DBException() {
-	}
+    public DBException() {
+    }
 
-	public DBException(String msg) {
-		super(msg);
-	}
-
-<<<<<<< HEAD
     public DBException(String msg) {
 	super(msg);
     }
@@ -21,9 +16,5 @@ public class DBException extends IPException {
     public DBException(String msg, Exception e) {
 	super(msg, e);
     }
-=======
-	public DBException(String msg, Exception e) {
-		super(msg, e);
-	}
->>>>>>> 8ffe6ca5b21b4e89a9c8e3668f519459a10e4f17
+
 }
--- src/main/java/tc/lv/exceptions/DownloadException.java
+++ src/main/java/tc/lv/exceptions/DownloadException.java
@@ -1,15 +1,14 @@
 package tc.lv.exceptions;
 
 public class DownloadException extends IPException {
-	/**
+    /**
      * 
      */
-	private static final long serialVersionUID = -1733773955555351297L;
+    private static final long serialVersionUID = -1733773955555351297L;
 
-	public DownloadException() {
-	}
+    public DownloadException() {
+    }
 
-<<<<<<< HEAD
     public DownloadException(String msg) {
 	super(msg);
     }
@@ -17,13 +16,5 @@ public class DownloadException extends IPException {
     public DownloadException(String msg, Exception e) {
 	super(msg, e);
     }
-=======
-	public DownloadException(String msg) {
-		super(msg);
-	}
 
-	public DownloadException(String msg, Exception e) {
-		super(msg, e);
-	}
->>>>>>> 8ffe6ca5b21b4e89a9c8e3668f519459a10e4f17
 }
