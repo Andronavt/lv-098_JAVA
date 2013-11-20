4ffec89d5a4d4ade8ce1cdb088ec44e0854f68f4
Bohdan <bkolesnyk85@gmail.com>
2013-11-20 21:54:12 +0200

    c

---
+++ src/main/java/tc/lv/service/MapService.java
@@ -0,0 +1,16 @@
+package tc.lv.service;
+
+import java.util.List;
+
+import tc.lv.domain.IpAddress;
+
+public interface MapService {
+
+    public List<IpAddress> loadWhiteListOfCityByRange(int i1, int i2);
+
+    public List<IpAddress> loadBlackListOfCityByRange(int i1, int i2);
+
+    public List<IpAddress> loadWhiteListOfCountryByRange(int i1, int i2);
+
+    public List<IpAddress> loadBlackListOfCountryByRange(int i1, int i2);
+}
---
+++ src/main/java/tc/lv/service/implementation/MapServiceImpl.java
@@ -0,0 +1,40 @@
+package tc.lv.service.implementation;
+
+import java.util.List;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import tc.lv.domain.IpAddress;
+import tc.lv.service.MapService;
+
+public class MapServiceImpl implements MapService {
+
+    @Override
+    @Transactional
+    public List<IpAddress> loadBlackListOfCityByRange(int i1, int i2) {
+        try {
+            return null;
+        } catch (Exception e) {
+            
+        }
+        
+    }
+
+    @Override
+    @Transactional
+    public List<IpAddress> loadBlackListOfCountryByRange(int i1, int i2) {
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public List<IpAddress> loadWhiteListOfCityByRange(int i1, int i2) {
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public List<IpAddress> loadWhiteListOfCountryByRange(int i1, int i2) {
+        return null;
+    }
+}
