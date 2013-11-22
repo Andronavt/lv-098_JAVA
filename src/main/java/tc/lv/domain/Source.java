960efdf50de860a159cee6e780c776dd406342db
Andriy <andrew.trefiak@gmail.com>
2013-11-22 14:51:13 +0200

    deleted not usable query

--- src/main/java/tc/lv/domain/Source.java
+++ src/main/java/tc/lv/domain/Source.java
@@ -19,8 +19,7 @@ import javax.persistence.Table;
 @Table(name = "sources")
 @NamedQueries({ @NamedQuery(name = Source.FIND_ALL, query = Source.FIND_ALL_QUERY),
         @NamedQuery(name = Source.FIND_BY_NAME, query = Source.FIND_BY_NAME_QUERY),
-        // ------
-        @NamedQuery(name = Source.DELETE, query = Source.DELETE_QUERY) })
+        })
 public class Source {
 
     public static final String FIND_BY_NAME = "Source.findByName";
@@ -29,8 +28,6 @@ public class Source {
     public static final String FIND_ALL = "Source.findAll";
     public static final String FIND_ALL_QUERY = "SELECT s FROM Source s";
 
-    public static final String DELETE = "Source.delete";
-    public static final String DELETE_QUERY = "DELETE FROM Source s where s.sourceName  = ?1";
 
     public static final String WHITE_LIST = "whitelist";
     public static final String BLACK_LIST = "blacklist";
