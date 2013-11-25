82fd29c616783e75e567dcef296577598c9963ab
unknown <orecto@gmail.com>
2013-11-25 17:08:22 +0200

    pagenation 2

--- src/main/java/tc/lv/service/IpAddressService.java
+++ src/main/java/tc/lv/service/IpAddressService.java
@@ -1,14 +1,11 @@
 package tc.lv.service;
 
-import tc.lv.domain.IpAddress;
 import tc.lv.exceptions.IpAddressServiceException;
 
 public interface IpAddressService {
 
-	public boolean saveIpByStatus(String address, String status)
-			throws IpAddressServiceException;
+    public boolean saveIpByStatus(String address, String status) throws IpAddressServiceException;
 
-	public boolean deleteIpByAddress(String address)
-			throws IpAddressServiceException;
+    public boolean deleteIpByAddress(String address) throws IpAddressServiceException;
 
 }
