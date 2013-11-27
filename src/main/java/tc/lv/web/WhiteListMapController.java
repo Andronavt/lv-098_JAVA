7d4c5d75faddeeae312952be23ded7377cda3201
Bohdan <bkolesnyk85@gmail.com>
2013-11-27 19:21:16 +0200

    Spring Security in Controller Methods

--- src/main/java/tc/lv/web/WhiteListMapController.java
+++ src/main/java/tc/lv/web/WhiteListMapController.java
@@ -1,15 +1,16 @@
 package tc.lv.web;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class WhiteListMapController {
-
     
+    @PreAuthorize("hasRole('ROLE_USER')")
     @RequestMapping(value = "secure_whiteListMap", method = RequestMethod.GET)
-    public String deleteIpFromWhiteList() {
+    public String showMapWhiteList() {
         return "secure_whiteListMap";
     }
     
